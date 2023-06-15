6cd33015e95ffe53ba2ab93dc0818dad5345a1d4
Deuteriunt <1619167551@qq.com>
2023-06-15 21:07:17 +0800

    main

---
+++ Dataserver/mitmdump/a.bat
@@ -0,0 +1,6 @@
+@echo off
+title 代理页面（勿删挂后台即可）By abigwhale 3.3
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f >nul 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "127.0.0.1:8080" /f >nul 2>nul
+
+mitmdump.exe -s a.py -k
